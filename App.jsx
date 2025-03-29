63d14eede4f6b956ee12c5a4fac762f8fec4e7c1
DERMOFEST <dundargroup@gmail.com>
2025-03-29 06:00:44 +0300

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,3 +1,16 @@
-export default function App() {
-  return <h1>DermoFest Ana Sayfa</h1>;
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
+export default App;
