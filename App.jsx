20d8faaa857eb1c615a35b4c6071bc4829a0713a
DERMOFEST <dundargroup@gmail.com>
2025-03-29 05:39:05 +0300

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,8 +1,3 @@
 export default function App() {
-  return (
-    <div style={{padding: 20}}>
-      <h1>Dermofest Ana Sayfa</h1>
-      <p>Bu alan eczacılar ve kullanıcılar için ürün listesini gösterecek.</p>
-    </div>
-  );
+  return <h1>Dermofest yayında!</h1>;
 }
