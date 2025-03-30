ac27ad8d268f90e9cd195cdf1aff58e95a50bd41
DERMOFEST <dundargroup@gmail.com>
2025-03-30 13:40:25 +0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import Register from "./pages/Register";
 import LogoBileseni from "./LogoBileseni";
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<LogoBileseni />} />
+      <Route path="/home" element={<Home />} />
       <Route path="/register" element={<Register />} />
     </Routes>
   );
--- src/LogoBileseni.jsx
+++ src/LogoBileseni.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import logo from "/logo.svg";
 
-export default function LogoBileşeni() {
+export default function LogoBileseni() {
   return (
     <div style={{
       display: "flex",
       flexDirection: "column",
       alignItems: "center",
-      marginTop: "50px"
+      justifyContent: "center",
+      minHeight: "100vh"
     }}>
       <img src={logo} alt="Dermofest Logo" width="180" />
       <h1>DERMOFEST</h1>
