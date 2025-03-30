f0e8f6435370bf19d5d746cdc629a8cb0a6621ee
DERMOFEST <dundargroup@gmail.com>
2025-03-30 12:32:30 +0300

    Add files via upload

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://your-project.supabase.co";
-const supabaseKey = "public-anon-key";
+const supabaseUrl = "https://ryiuqghjgmofhfcwimzb.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5aXVxZ2hqZ21vZmhmY3dpbXpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE4MDkxODgsImV4cCI6MjAyNzU4NTE4OH0.rXrwGzS1rC5SFFDECDX4JXyFHLb8AJbRMcvAD0jQTP0";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
